2c88acaeb52de0e0452c1bd1e129a621670bfd3d
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-03-03 12:59:48 -0600

    finished part 1 homework with the intention of returning for the stretch goals before submitting

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,43 +1,85 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 
+const firstName = 'Chris ';
+
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+
+const lastName = 'Boyd'; 
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 
+const fullName = firstName + lastName;
+
 // 4 - Console log the value of `fullName`
 
+console.log( fullName );
+
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
+
+let luckyNumber = 11;
 
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
 
+console.log( `My name is ${fullName} and I think ${luckyNumber} is a winner!` ); /* Chris here! Just showing off (while I still can) with what I think is a slightly slicker way to make a sentence using variables. Note the backticks instead of single or double quotes as they are required when using template literals. */
+
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+
+let adventurous = true; //I am going to Prime, after all.
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 
+let food = 'pizza';
+
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
+
+let pets = 1;
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 
+let friendsPets = 2;
+
 // 11 - Add two pets to your `pets` variable
 
+friendsPets =+ 2;
+
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+
+allowedPets = 3;
 
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
 
+if(adventurous){
+    console.log( 'Adventures are great!' );
+}
+else{
+    console.log( 'How about we stay home?' );
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+
+if(luckyNumber == 2 && adventurous){
+    console.log( 'Roll the dice!' );
+}
 
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+
+if(pets < allowedPets){
+    console.log( 'I can have more pets!' );
+} else if(pets === allowedPets){
+    console.log( 'I have enough pets' );
+} else if(pets > allowedPets){
+    console.log( 'Oh no, I have too many pets!' );
+}
 
 // STRETCH GOALS:
 
@@ -55,5 +97,3 @@
 //      You'll need to research how to use switch statements!
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-
-
